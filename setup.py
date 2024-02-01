3077babec1d8ae1822c45c870f10cce9fbd863d9
Goodnight <mohammedarbi.nsibi@gmail.com>
2024-02-01 20:28:54 +0100

    utils added

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md",  "r", encoding= "utf-8") as f:
     long_description = f.read()
 
 
+__version__ = "0.0.0"
 
-'__version__' == "0.0.0"
 
-REPO_NAME="Kidney-Disease-Classification-MLflow-DVC"
+REPO_NAME="Goodnight77-Kidney-Disease-Classification-MLflow-DVC"
 AUTHOR_USER_NAME ="Goodnight77"
 SRC_REPO= "cnnClassifier"
 AUTHOR_EMAIL= "mohammedarbi.nsibi@gmail.com"
@@ -15,7 +15,7 @@ AUTHOR_EMAIL= "mohammedarbi.nsibi@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
-    version='__version__',
+    version= __version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
